41f1aa340dcf385cd67e00fcb86232a627eac0ff
Roger Yulier Bello Romero <RYBR@Bullwinkle.dhcp.lbl.gov>
2020-03-05 16:44:46 -0800

    No era esto

--- orb.h
+++
@@ -1,7 +0,0 @@
-#ifndef HEADER_ORB_H
-#define HEADER_ORB_H
-
-void _CHANGE_STR(char *str,double *val);
-void _getorb(char *fichero,int l,int ngrid,double *xi,double *yi,double *zi,double *mo_orb);
-
-#endif
